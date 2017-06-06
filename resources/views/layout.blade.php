f77bda24dc967889e58ac471859398f6b088139d
Junior Nunes <edmilson.junior@invate.com.br>
2017-06-06 16:31:18 -0300

    csrf-token no meta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Tasks List</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
